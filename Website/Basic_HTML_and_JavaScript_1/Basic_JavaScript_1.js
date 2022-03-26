60b0f1471325d5bd3a3859a1741766a078066902
altrdstates <100340019+altrdstates@users.noreply.github.com>
2022-03-25 17:33:54 -0700

    Basic HTML and JavaScript
    alt attribute
    <img>element
    <b>tag
    <i>tag
    <u>tag

---
+++ Website/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+function My_First_Function()    {
+        var String = "HeeHeeHee stop I'm ticklish!";
+        var result = String.fontcolor("green");
+        document.getElementById("Poke").innerHTML = result;
+    }
